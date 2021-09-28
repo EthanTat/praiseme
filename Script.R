16e93c0c8856cb584d3c7ce88d27ec6d1ddf3cc5
Ethan Tat <ethan.tat@students.mq.edu.au>
2021-09-28 16:40:45 +1000

    Initial commit

---
+++ Script.R
@@ -0,0 +1,21 @@
+#Edit
+usethis::use_r("praise")
+
+#Checking package working
+devtools::load_all()
+devtools::check()
+
+#Lisences
+ usethis::use_mit_license("Ethan Tat")
+devtools::document()
+?praise
+usethis::use_package("glue")
+
+#Testing
+usethis::use_testthat()
+
+usethis::use_test("praise")
+
+devtools::test()
+
+devtools::test_coverage()
